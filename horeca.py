62f5583a3bade670f6e56ca9c4d25f15696433c9
KC <93526857+Consiglieri-K@users.noreply.github.com>
2021-12-21 09:28:25 +0100

    FINALFINAL

--- horeca.py
+++ horeca.py
@@ -1,27 +1,30 @@
+#import benodigheden
 import csv
 import os
 from datetime import datetime
 
+#variabelen
 horecaFile = open("GH.csv", "r", encoding = "UTF-8")
 reader = csv.DictReader(horecaFile, delimiter=";")
 horecaList = list(reader)
 outFile = open("outFile.txt", "a")
-
-
-#NIGHTVALUE 
 nightValue = 0
 isRunning = True
+
+#programma/ loop
 while isRunning:
 
+    #keuzemenu
     print("1. Bereken het gemiddeld aantal bezoekers binnen.")
     print("2. Bereken alle horeca ondernemingen met meer dan 50 plekken op het terras.")
     print("3. Bereken de hoeveelheid horecaondernemingen met een nachtvergunning.")
     print("4. Maak een top-10 lijst van de oudste horecaondernemingen met vergunningen. ")
     print("\n Druk op X om te stoppen")
-    
+
     choice = input("\n Maak uw keuze:")
     os.system("cls")
 
+    #optie 1.
     if choice == "1":
         totalValue = 0
         for inside in horecaList:
@@ -31,7 +34,7 @@ while isRunning:
         print(f"Totaal aantal bezoekers is: {totalValue} \n")
         outFile.write(f"Totaal aantal bezoekers is: {totalValue} \n")
         
-
+    #optie 2.
     elif choice == "2":
 
         print("Aantal horecaondernemingen met meer dan 50 plekken op hun terras: ")
@@ -45,7 +48,7 @@ while isRunning:
         print(f"Aantal horecaondernemingen met meer dan 50 mensen = {terras_horeca} \n")
         outFile.write(f"Aantal horecaondernemingen met meer dan 50 mensen = {terras_horeca} \n")
         
-
+    #optie 3.
     elif choice == "3":
         for nightLicense in horecaList:
             if nightLicense["nachtvergunning"] == "Ja":
@@ -55,8 +58,7 @@ while isRunning:
         print(f"Totaal aantal horeca met een nachtvergunning: {nightValue} \n")    
         outFile.write(f"Totaal aantal horeca met een nachtvergunning: {nightValue} \n")  
        
-
-    
+    #optie 4.
     elif choice == "4":
         top10 = []
         i = 0
@@ -78,11 +80,12 @@ while isRunning:
         outFile.write(f"Top 10 oudste vergunningen: \n {list}")
         print("-------------------------------------- \n")
         outFile.write("-------------------------------------- \n")
-        
+
+    #stop programma
     elif choice.upper() == "X":
         isRunning = False
     
-
+#sluit bestanden
 horecaFile.close()
 outFile.close()
 
